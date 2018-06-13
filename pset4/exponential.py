ac31d623aee4ad5bfec7e3f01d655556a64831e9
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-13 17:12:58 -0400

    Add files via upload

---
+++ pset4/exponential.py
@@ -0,0 +1,13 @@
+# exponential.py
+
+# Name :
+# Collaborators :
+
+# This program writes our own version of 
+# exponential, **
+
+def exponential(base, exp):
+	# add your code here for calculating the final result
+	return result
+	
+# ask for user input, then call the exponential function
