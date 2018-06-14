80d4c6622ff4603b14c691c5a5e1436c3b40b434
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-14 18:05:21 -0400

    Add files via upload

---
+++ pset6/create.py
@@ -0,0 +1,7 @@
+# create.py
+
+# Name :
+# Collaborators :
+
+# This practices creating an object and a class.
+
---
+++ pset6/wheel.py
@@ -0,0 +1,42 @@
+# wheel.py
+
+# Name :
+# Collaborators :
+
+# Program for drawing a moving wheel
+
+from graphics import *
+
+class Wheel(object):
+        def __init__(self, center, wheel_radius, tire_radius):
+                self.tire_circle = Circle(center, tire_radius)
+                self.wheel_circle = Circle(center, wheel_radius)
+        def draw(self, win):
+                self.tire_circle.draw(win)
+                self.wheel_circle.draw(win)
+        def move(self, dx, dy):
+                self.tire_circle.move(dx, dy)
+                self.wheel_circle.move(dx, dy)
+        def set_color(self, wheel_color, tire_color):
+                self.tire_circle.setFill(tire_color)
+                self.wheel_circle.setFill(wheel_color)
+        def undraw(self):
+                self.tire_circle.undraw()
+                self.wheel_circle.undraw()
+        def get_size(self):
+                return self.tire_circle.getRadius()
+        def get_center(self):
+                return self.tire_circle.getCenter()
+        def animate(self, win, dx, dy, n):
+                for counter in range(n):
+                        time.sleep(.1)
+                        self.move(dx, dy)
+
+win = GraphWin('Wheel', 320, 240)
+w = Wheel(Point(100, 100), 50, 70)
+w.draw(win)
+w.set_color('gray', 'black')
+w.animate(win, 1, 0, 100)
+#w.undraw()
+
+win.mainloop()
