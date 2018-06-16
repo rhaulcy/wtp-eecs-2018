a5b2e86746c4a489aebee6dd9e2f0881b52e1fb6
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-16 14:07:38 -0400

    Create blackjack.py

---
+++ pset3/blackjack.py
@@ -0,0 +1,14 @@
+# blackjack.py
+
+# Name:
+# Collaborators: 
+
+# Fill in the table below
+
+# | card1 | card2 | dealer_card | output |
+# | ----- | ----- | ----------- | ------ |
+# | 3     | 8     | 4           |        |
+# | 9     | 8     | 10          |        |
+# | 11    | 10    | 10          |        |
+# | 3     | 5     | 4           |        |
+# | 5     | 8     | 8           |        |
