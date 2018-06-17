cc20034a9878785afa7e2610da9991f4186b0e7e
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-17 18:43:13 -0400

    Create hangman.py

---
+++ hangman/day1/hangman.py
@@ -0,0 +1,13 @@
+# hangman.py
+
+# Name:
+# Collaborators: 
+
+def display(letter, word):
+    """
+    :param letter: a list of unique characters in word
+    :param word: a string
+    :return: a string that displays word using characters in letters and underscores for any missing characters 
+        with spaces between each character
+    """
+    pass
