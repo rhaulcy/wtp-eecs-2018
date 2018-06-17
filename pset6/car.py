45f0495db8c8606af04076183015fc05515091f2
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-16 20:24:45 -0400

    Update car.py

--- pset6/car.py
+++ pset6/car.py
@@ -8,7 +8,7 @@
 from graphics import *
 
 win = GraphWin("Car", 300, 300)
-rect = Rectangle(Point(10,10), Point(200,10))
+rect = Rectangle(Point(10,10), Point(100,100))
 
 rect.setFill("blue")
 rect.draw(win)
