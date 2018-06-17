0eabffd50ee1697059fec05c008c3bd5695b6354
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-17 18:57:28 -0400

    Create drawing.py

---
+++ hangman/day2/drawing.py
@@ -0,0 +1,40 @@
+from graphics import *
+
+"""
+Example Code
+===============================================================================
+win = GraphWin("Example", 200, 200)
+a = Line(Point(50, 0), Point(50, 200))
+b = Circle(Point(125, 50), 25)
+c = Rectangle(Point(100, 125), Point(175, 175))
+a.setWidth(10)
+b.setFill("blue")
+c.setOutline("red")
+a.draw(win)
+b.draw(win)
+c.draw(win)
+===============================================================================
+"""
+
+#Hangman Graphics: make your window here
+
+
+#Scaffold code here
+
+
+#Head code here
+
+
+#Torso code here
+
+
+#Left Leg code here
+
+
+#Right Leg code here
+
+
+#Left Arm code here
+
+
+#Right Arm code here
