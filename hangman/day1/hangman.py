04e86884298ab7197dd1ab5fabf8afd8144cf6b2
rhaulcy <35573241+rhaulcy@users.noreply.github.com>
2018-06-18 13:13:44 -0400

    Update hangman.py

--- hangman/day1/hangman.py
+++ hangman/day1/hangman.py
@@ -5,7 +5,7 @@
 
 def display(letter, word):
     """
-    :param letter: a list of unique characters in word
+    :param letter: a list of the letters the user has guessed
     :param word: a string
     :return: a string that displays word using characters in letters and underscores for any missing characters 
         with spaces between each character
